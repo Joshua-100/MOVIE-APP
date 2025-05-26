905b6cf17facbad427d191c4b3e52a597c2f09da
Joshua <mutenjejoshua@gmail.com>
2025-05-26 11:26:24 +0200

    Working Page Routers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import MovieCard from './components/MovieCard'
 import Home from './pages/Home'
+import Favorites from './pages/Favorites'
 
 function App() {
   return (
-    <>
-      <Home/>
-    </>
+    <main className='main-content'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/favorites' element={<Favorites/>}/>
+      </Routes>
+    </main>
   )
 }
 
 
 
-export default App
+export default App;
