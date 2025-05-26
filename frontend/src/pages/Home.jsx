21f826709fd29e02aea60a30bd0e162e97b3ca63
Joshua <mutenjejoshua@gmail.com>
2025-05-26 11:12:59 +0200

    Added react-router-dom

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
 function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+
     const movies = [
         {id: 1, title: "John Wick", release_date: "2019"},
         {id: 2, title: "John Cage", release_date: "2015"},
@@ -8,16 +11,25 @@ function Home() {
         {id: 4, title: "Shangchi", release_date: "2021"}
     ];
 
-    const handleSearch = () => {};
+    const handleSearch = (e) => {
+        e.preventDefault();
+        alert(searchQuery);
+    };
 
     return (
         <div className="home">
             <form onSubmit={handleSearch} className="search-form">
-                <input type="text" placeholder="Search for movies..." className="search-input"/>
+                <input 
+                type="text" 
+                placeholder="Search for movies..." 
+                className="search-input"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}                
+                />
                 <button type="submit" className="search-button">Search</button>
             </form>
             <div className="movies-grid">
-                {movies.map((movie) => (
+                {movies.map((movie) => ( movie.title.toLowerCase().includes(searchQuery) &&
                     <MovieCard movie={movie} key={movie.id}/>                    
                 ))}
             </div>
